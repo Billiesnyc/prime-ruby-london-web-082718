deb15b96e69af4a94d78104ab55535b0de961a60
Billie Simmons <billiesnyc@gmail.com>
2018-08-14 11:11:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 def prime?(i)
-  return false if i <= 1
-  return false if i <= 0
+  loop do
+  if i <= 1
+    false
+  elsif i <= 0
+    false
+  elsif
   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 end
